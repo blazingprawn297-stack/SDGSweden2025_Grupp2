d0d9b18646f33a1eadac51eddebfb961f7b9b4f5
blazingprawn297-stack <blazingprawn297@gmail.com>
2025-12-30 13:05:38 +0100

    Update SDGSweden.java

--- src/SDGSweden2025/SDGSweden.java
+++ src/SDGSweden2025/SDGSweden.java
@@ -4,10 +4,28 @@
  */
 package SDGSweden2025;
 
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
 /**
  *
  * @author Bilda
  */
 public class SDGSweden {
+
     
+    private static InfDB idb;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    // Hejdå
 }
+
